e40bfe250c0cc57070c6ec4b812890bbf7c4eaab
Eduardo Rosendo <eduardojra96@gmail.com>
2022-07-24 21:04:21 -0400

    add static files url to list of endpoints

--- engine/engine/urls.py
+++ engine/engine/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.urls import include, re_path
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     re_path('admin/', admin.site.urls),
     re_path(r'^api/',  include(('api.urls', 'api'), namespace='api')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
